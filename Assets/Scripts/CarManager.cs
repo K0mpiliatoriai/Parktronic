abf924ef94f219132a7c06ab5e1d4cd8d4ffcc9c
Aruunce <118486133+Aruunce@users.noreply.github.com>
2023-04-17 02:44:46 +0300

    feat: Added park button
    When car is parked, button pops and if it's clicked level ends.

--- Assets/Scripts/CarManager.cs
+++ Assets/Scripts/CarManager.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class CarManager : MonoBehaviour
 {
     public static bool IsGameFailed;
+    public static bool isPClicked;
     public GameObject gameFailedScreen;
     public GameObject gameWonScreen;
     public GameObject healthBar;
     public GameObject pauseButton;
+    public GameObject parkButton;
     public static bool LowerRightPoint;
     public static bool UpperLeftPoint;
     public static bool UpperRightPoint;
@@ -17,32 +19,33 @@ public class CarManager : MonoBehaviour
 
     private void Awake()
     {
-        IsGameFailed= false;
+        IsGameFailed = false;
         LowerLeftPoint = false;
         UpperLeftPoint = false;
         LowerRightPoint = false;
         UpperRightPoint = false;
+        isPClicked = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(IsGameFailed)
+        if (IsGameFailed)
         {
             Time.timeScale = 0;
             gameFailedScreen.SetActive(true);
             healthBar.SetActive(false);
             pauseButton.SetActive(false);
         }
-        else if (LowerLeftPoint && UpperLeftPoint && LowerRightPoint && UpperRightPoint)
+        else if (LowerLeftPoint && UpperLeftPoint && LowerRightPoint && UpperRightPoint && !isPClicked)
         {
-            Time.timeScale = 0;
-            gameWonScreen.SetActive(true);
-            healthBar.SetActive(false);
-            pauseButton.SetActive(false);
+            parkButton.SetActive(true);
+        }
+        else
+        {
+            parkButton.SetActive(false);
         }
     }
-
     public void ReplayLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
@@ -53,5 +56,13 @@ public class CarManager : MonoBehaviour
         gameWonScreen.SetActive(false);
         healthBar.SetActive(false);
         pauseButton.SetActive(false);
+    }
+    public void ParkCar()
+    {
+        Time.timeScale = 0;
+        gameWonScreen.SetActive(true);
+        healthBar.SetActive(false);
+        pauseButton.SetActive(false);
+        isPClicked = true;
     }
 }
