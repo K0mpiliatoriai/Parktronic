d4bb1e5a6934cdcd469ba5d1842a4c4ff296ba49
TautvisP <59112653+TautvisP@users.noreply.github.com>
2023-05-01 12:47:09 +0300

    Fixed a bug

--- Assets/Scripts/CollectableObject.cs
+++ Assets/Scripts/CollectableObject.cs
@@ -8,14 +8,24 @@ using UnityEngine.SceneManagement;
 public class CollectableObject : MonoBehaviour
 {
     [SerializeField] public TextMeshProUGUI collectablesText;
-    public int Value;
+    private bool isColliding = false;
     public void OnTriggerEnter2D(Collider2D collision)
     {
+        if (isColliding) return;
+        isColliding = true;
+
         if (collision.isTrigger)
         {
             Destroy(gameObject);
-            CarManager.CollectableCount+=Value;
+            CarManager.CollectableCount++;
             collectablesText.text = "Wrenches: " + CarManager.CollectableCount;
         }
+        StartCoroutine(Reset());
+    }
+
+    IEnumerator Reset()
+    {
+        yield return new WaitForEndOfFrame();
+        isColliding = false;
     }
 }
